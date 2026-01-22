775091e6b1ae8c8ff12d99e55a3fe0572a1f4d54
drAkEn-69 <faizzameer16@gmail.com>
2026-01-22 23:28:17 +0530

    default path

--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { SupabaseService } from '../storage/supabase.service';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class AssetsService {
